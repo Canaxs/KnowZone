30960606d01a8e5d8e4338ee4fdbc17549940018
Meriç Cana <mmericcana@gmail.com>
2025-07-19 17:34:53 +0300

    Initialize Spring Boot project with base configuration

---
+++ src/main/java/com/knowzone/KnowzoneApplication.java
@@ -0,0 +1,13 @@
+package com.knowzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowzone/KnowzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
